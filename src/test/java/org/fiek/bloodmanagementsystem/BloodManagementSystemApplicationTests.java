df16bea509ed6b13cdb2ab28dcd87883ae113225
Avdirrahman Gegaj <avdirrahman.gegaj@studentet.uni-pr.edu>
2019-12-10 00:43:15 +0100

    DB Connection

---
+++ src/test/java/org/fiek/bloodmanagementsystem/BloodManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fiek.bloodmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
